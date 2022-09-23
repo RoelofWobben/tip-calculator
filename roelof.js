9fe11af3d40f39497cf50ab666517f4c1c9d5d9a
RoelofWobben <r.wobben@home.nl>
2022-09-23 11:09:46 +0200

    error message not shown

--- roelof.js
+++ roelof.js
@@ -1,6 +1,7 @@
 let money = document.querySelector('#amount');
 let money_input = document.querySelector('#amount-input')
-
+let error = document.querySelector(".errormessage");
+let buttons = document.querySelectorAll(".buttons"); 
 
 class TipCalculator{
 
@@ -9,38 +10,6 @@ class TipCalculator{
         this.tip = tip; 
         this.numberOfPersons = persons ; 
     }
-
-    //Validation functions
-    
-    //Validate format amount 
-
-    validateFormatAmount = () => {
-        var reg = /(?=.*?\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|\d+)?(\.\d{1,2})?$/;
-        return reg.test(this.amount);
-         
-    }
-
-    isNotNull = () => {
-        return parseFloat(this.amount) !== 0 ; 
-    }
-
-    //Display Error Messages 
-    displayErrorMessage =  (error) => {
-        let errorClass = document.createElement('error');
-        errorClass.innerHTML = error;
-        errorClass.insertBefore('money_input');  
-    }
-
-    //validate amount input 
-
-    validateAmount = () => {
-        if (!this.validateFormatAmount() && this.isNotNull()) {
-            console.log('error message');
-            this.displayErrorMessage('invalid input'); 
-        }; 
-    }
-
-    
 }
 
 let calculator =  new TipCalculator(0.00, 0.00, 1); 
@@ -50,13 +19,15 @@ let calculator =  new TipCalculator(0.00, 0.00, 1);
 money.addEventListener('blur', (e) => {
     // set entered amount in the class 
     calculator.amount = e.target.value; 
-    // Validate the input
-    calculator.validateAmount(); 
-
-})
+});
 
 //add eventListeners to the buttons
 
+buttons.forEach(function(button){
+    button.addEventListener('click', (e) => {
+        calculator.tip = e.target.value; 
+    })
+})
 
 
 
