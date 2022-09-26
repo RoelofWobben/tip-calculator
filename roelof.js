ec5af28983b0d2fc570d012b31086cae71039e43
RoelofWobben <r.wobben@home.nl>
2022-09-26 11:03:04 +0200

    a little bit further

--- roelof.js
+++ roelof.js
@@ -1,7 +1,11 @@
 let money = document.querySelector('#amount');
 let money_input = document.querySelector('#amount-input')
 let error = document.querySelector(".errormessage");
-let buttons = document.querySelectorAll(".buttons"); 
+let buttons = document.querySelectorAll(".buttons");
+let custom = document.querySelector('#percentage');
+let person = document.querySelector('#person');
+
+money.noValidate = true;
 
 class TipCalculator{
 
@@ -16,7 +20,15 @@ let calculator =  new TipCalculator(0.00, 0.00, 1);
 
 //add eventListener to the amount input 
 
-money.addEventListener('blur', (e) => {
+money.addEventListener('invalid', (e) => {
+    
+    //show error if needed
+    if (money.checkValidity()){
+        error.setAttribute('aria-invalid', 'false')
+    } else {
+        error.setAttribute('aria-invalid', 'true')
+    }
+
     // set entered amount in the class 
     calculator.amount = e.target.value; 
 });
@@ -25,10 +37,21 @@ money.addEventListener('blur', (e) => {
 
 buttons.forEach(function(button){
     button.addEventListener('click', (e) => {
-        calculator.tip = e.target.value; 
+        calculator.tip = e.target.value;
+        //add class to selected button 
+
     })
 })
 
+percentage.addEventListener('enter', (e) => {
+    calculator.tip = e.target.value;
+     
+}); 
+
+person.addEventListener('blur', (e) =>{
+   calculator.numberOfPersons = e.target.value; 
+ 
+})
 
 
 
