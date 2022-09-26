69bec50f34b71e88476bf2afc4ba9a6a27248bfe
RoelofWobbenfrontend <104582123+RoelofWobbenfrontend@users.noreply.github.com>
2022-09-26 14:55:48 +0200

    added comments to the javascript

--- roelof.js
+++ roelof.js
@@ -1,3 +1,5 @@
+
+// Methods to make the html avaible in javascript 
 let money = document.querySelector('#amount');
 let money_input = document.querySelector('#amount-input')
 let error = document.querySelector(".errormessage");
@@ -5,8 +7,8 @@ let buttons = document.querySelectorAll(".buttons");
 let custom = document.querySelector('#percentage');
 let person = document.querySelector('#person');
 
-money.noValidate = true;
 
+// Class to hold the variables and functions 
 class TipCalculator{
 
     constructor(amount, tip, persons){
@@ -20,33 +22,34 @@ let calculator =  new TipCalculator(0.00, 0.00, 1);
 
 //add eventListener to the amount input 
 
-money.addEventListener('invalid', (e) => {
-    
-    //show error if needed
-    if (money.checkValidity()){
-        error.setAttribute('aria-invalid', 'false')
-    } else {
-        error.setAttribute('aria-invalid', 'true')
-    }
 
-    // set entered amount in the class 
-    calculator.amount = e.target.value; 
-});
+
+// Add invalid eventListener so the error message can be shown or hidden after a (in)valid input 
+money.addEventListener('invalid', (e) => {
+    console.log('invalid')
+    //show error because aria-live is set to true 
+    money.setAttribute('aria-invalid', 'true')
+}); 
+
+   
 
 //add eventListeners to the buttons
 
 buttons.forEach(function(button){
-    button.addEventListener('click', (e) => {
+    button.addEventListener('change', (e) => {
         calculator.tip = e.target.value;
         //add class to selected button 
 
     })
 })
 
+// add eventListener to the little inout field next to the buttons
 percentage.addEventListener('enter', (e) => {
     calculator.tip = e.target.value;
      
 }); 
+
+//Add eventListeners to the person input field
 
 person.addEventListener('blur', (e) =>{
    calculator.numberOfPersons = e.target.value; 
